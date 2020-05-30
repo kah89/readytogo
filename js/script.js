acc44957fb659086f54b64fbcfb4a2bd2299a830
karina <krs_souza@yahoo.com.br>
2020-05-30 11:36:56 -0300

    primeiro projeto

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* ação + Escola. Tela Cadastro aut */
+
+function duplicarCampos(){
+	var clone = document.getElementById('origem').cloneNode(true);
+	var destino = document.getElementById('destino');
+	destino.appendChild (clone);
+	
+	var camposClonados = clone.getElementsByClassName('campo');
+	
+	for(i=0; i<camposClonados.length;i++){
+		camposClonados[i].value = '';
+	}
+	
+	
+	
+}
+
+function removerCampos(id){
+	var node1 = document.getElementById('destino');
+	node1.removeChild(node1.childNodes[0]);
+}
